b19d211ecc6359e639ce9394028482693b264043
ALAN-SVV <alansvela2019@gmail.com>
2025-11-14 11:47:13 -0500

    ngfor y ngif

--- src/app/component/estudiantes/estudiantes.component.ts
+++ src/app/component/estudiantes/estudiantes.component.ts
@@ -1,85 +1,98 @@
 import { Component } from '@angular/core';  
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
+
+interface Estudiante {
+  nombre: string;
+  apellido: string;
+  cedula: number;
+  carrera: string;
+  notas: number;
+}
 
 @Component({
   selector: 'app-estudiantes',
-  imports: [ FooterComponent, NavbarComponent],
+  imports: [ FooterComponent, NavbarComponent, CommonModule],
   templateUrl: './estudiantes.component.html',
   styleUrl: './estudiantes.component.css'
 })
+
+
 export class EstudiantesComponent {
-estudiante1={
+estudiantes: Estudiante[]=[
+  {
     nombre:"Maria",
     apellido:"Gonzalez",
     cedula:1102456789,
     carrera:'Ingeniería en sistemas',
-    notas:88
-  };
-estudiante2={
+    notas:50
+  },
+  {
     nombre:"Carlos",
     apellido:"Rodriguez",
     cedula:1102345678,
     carrera:'Administración',
-    notas:92,
-  };
-estudiante3={
+    notas:40
+  },
+  {
     nombre:"Ana",
     apellido:"Martinez",
     cedula:1102234567,
     carrera:'Medicina',
     notas:76,
-  };
-estudiante4={
+  },
+  {
     nombre:"Luis",
     apellido:"Perez",
     cedula:1102123456,
     carrera:'Derecho',
     notas:85,
-  };
-estudiante5={
+  },
+  {
     nombre:"Sofia",
     apellido:"Hernandez",
     cedula:1102012345,
     carrera:'Psicología',
     notas:90,
-  };
-estudiante6={
+  },
+  {
     nombre:"Diego",
     apellido:"Ramirez",
     cedula:1102987654,
     carrera:'Arquitectura',
-    notas:79,
-  };
-estudiante7={
+    notas:39,
+  },
+  {
     nombre:"Elena",
     apellido:"Torres",
     cedula:1102876543,
     carrera:'Contabilidad',
     notas:87,
-  };
-estudiante8={
+  },
+{
     nombre:"Javier",
     apellido:"Diaz",
     cedula:1102765432,
     carrera:'Marketing',
-    notas:83,
-  };
-estudiante9={
+    notas:69,
+  },
+{
     nombre:"Paula",
     apellido:"Sanchez",
     cedula:1102654321,
     carrera:'Enfermería',
     notas:91,
-  };
-estudiante10={
+  },
+  {
     nombre:"Miguel",
     apellido:"Castillo",
     cedula:1102543210,
     carrera:'Educación',
-    notas:74,
-  };
-
+    notas:64,
+  },
+  
+]
 
   MostrarDatos(estudiante: any){ // Metodo para mostrar los datos, any es para el tipo de dato
   console.log('Nombre de usuario:', estudiante);
--- src/app/component/profes/profes.component.ts
+++ src/app/component/profes/profes.component.ts
@@ -1,37 +1,69 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { FooterComponent } from "../footer/footer.component";
-import { EstudiantesComponent } from '../estudiantes/estudiantes.component';
+import { CommonModule } from '@angular/common'; 
+import { UrlCodec } from '@angular/common/upgrade';
+
+interface Usuario {
+  nombre: string;
+  profesion: string;
+  descripcion: string;
+  redes?: { nombre: string; icono: string; link: string }[];
+  img: string;
+}
 
 @Component({
   selector: 'app-profes',
-  imports: [NavbarComponent,FooterComponent, EstudiantesComponent],
+  imports: [NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './profes.component.html',
   styleUrl: './profes.component.css'
 })
+
 export class ProfesComponent {
-  usuario1={
+  usuarios: Usuario[]=[
+{
     nombre:"Elvis Pachacama",
     profesion:'ingeniero de sistemas',
     descripcion:'Apasionado de la IA',
-  };
-  usuario2={
+    img: "images/elvis.png"
+   
+  },
+  {
     nombre:'Gabriel Hoyos',
     profesion:'Desarollo de software',
     descripcion:'Amante del codigo limpio y soluciones eficientes',
+    img: "images/gabo.jpg",
+   
     redes:[
       {nombre: 'github', icono:'fab fa-github', link: '#'},
       {nombre: 'stack-overflow', icono:'fab fa-stack-overflow', link: '#'}
-    ]
-  };
-  usuario3={
+    ],
+    
+  },
+  {
     nombre:'Monica Ramirez',
     profesion:'Analisis de datos',
     descripcion:'Experta ene ncontrara patrones y extraer informacion valiosa.',
-  };
+    img: "images/moni.jpg"
+  },
+  {
+    nombre:'Monica Ramirez',
+    profesion:'Analisis de datos',
+    descripcion:'Experta ene ncontrara patrones y extraer informacion valiosa.',
+    img: ""
+  },
+  {
+    
+    nombre:'Monica Ramirez',
+    profesion:'Analisis de datos',
+    descripcion:'Experta ene ncontrara patrones y extraer informacion valiosa.',
+    img: ""
+  }
+ ]
 
   mostrarDatos(usuario: any){ // Metodo para mostrar los datos, any es para el tipo de dato
   console.log('Nombre de usuario:', usuario);
   }
-  
+  mostrarRedesSociales: boolean = true;
 }
+
